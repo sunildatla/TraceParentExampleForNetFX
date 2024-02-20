c4659a3e0178f7069e09daa8d3683f20a999d042
sunildatla <sunilmdatla@hotmail.com>
2024-02-20 13:59:57 -0800

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Mvc;
+
+namespace TraceParentExampleForNetFX.Controllers
+{
+    public class HomeController : Controller
+    {
+        ActivitySource source = new ActivitySource("TraceParentExampleForNetFX");
+        public ActionResult Index()
+        {
+            var activity= source.StartActivity("HomeController.Index");
+           
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage responseMessage = client.GetAsync("https://api.publicapis.org/entries").GetAwaiter().GetResult();
+
+            responseMessage.RequestMessage.Headers.TryGetValues("traceparent", out IEnumerable<string> traceparent);
+            if(traceparent != null)
+            {
+                activity?.SetTag("TraceParent", traceparent.FirstOrDefault());
+
+            }
+            activity?.SetTag("customTag", "customValue");
+            activity?.Stop();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Trace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace TraceParentExampleForNetFX
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Sdk.CreateTracerProviderBuilder().AddAspNetInstrumentation().AddHttpClientInstrumentation().AddSource("*").AddConsoleExporter().Build();
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
